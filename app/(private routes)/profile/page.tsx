00982303a0333293056df9aec1437c34598f31da
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-03-09 22:51:01 +0200

    Find a problem with vercel

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,51 +1,51 @@
-import css from "./ProfilePage.module.css";
-import { Metadata } from "next";
-import Link from "next/link";
-import { getMe } from "@/lib/api/serverApi";
-import Image from 'next/image';
+// import css from "./ProfilePage.module.css";
+// import { Metadata } from "next";
+// import Link from "next/link";
+// import { getMe } from "@/lib/api/serverApi";
+// import Image from 'next/image';
 
-export const metadata: Metadata = {
-    title: "NoteHub: Profile page",
-    description: "NoteHub user profile page",
-    openGraph: {
-      title: "NoteHub: Profile page",
-      description: "NoteHub user profile page",
-      url: "https://notehub.com/profile",
-      images: [{
-        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
-        width: 1536,
-        height: 1024,
-        alt: "NoteHub logo"
-      },
-    ],
-    }
-};
+// export const metadata: Metadata = {
+//     title: "NoteHub: Profile page",
+//     description: "NoteHub user profile page",
+//     openGraph: {
+//       title: "NoteHub: Profile page",
+//       description: "NoteHub user profile page",
+//       url: "https://notehub.com/profile",
+//       images: [{
+//         url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+//         width: 1536,
+//         height: 1024,
+//         alt: "NoteHub logo"
+//       },
+//     ],
+//     }
+// };
 
-export default async function ProfilePage() {
-  const user = await getMe();
-  return (
-    <main className={css.mainContent}>
-      <div className={css.profileCard}>
-        <div className={css.header}>
-          <h1 className={css.formTitle}>Profile Page</h1>
-          <Link href="/profile/edit" className={css.editProfileButton}>
-            Edit Profile
-          </Link>
-        </div>
-        <div className={css.avatarWrapper}>
-          <Image
-            src={`${user?.avatar}`}
-            alt="User Avatar"
-            width={120}
-            height={120}
-            className={css.avatar}
-          />
-        </div>
-        <div className={css.profileInfo}>
-          <p>Username: {user?.username}</p>
-          <p>Email: {user?.email}</p>
-        </div>
-      </div>
-    </main>
-  );
-}
+// export default async function ProfilePage() {
+//   const user = await getMe();
+//   return (
+//     <main className={css.mainContent}>
+//       <div className={css.profileCard}>
+//         <div className={css.header}>
+//           <h1 className={css.formTitle}>Profile Page</h1>
+//           <Link href="/profile/edit" className={css.editProfileButton}>
+//             Edit Profile
+//           </Link>
+//         </div>
+//         <div className={css.avatarWrapper}>
+//           <Image
+//             src={`${user?.avatar}`}
+//             alt="User Avatar"
+//             width={120}
+//             height={120}
+//             className={css.avatar}
+//           />
+//         </div>
+//         <div className={css.profileInfo}>
+//           <p>Username: {user?.username}</p>
+//           <p>Email: {user?.email}</p>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
