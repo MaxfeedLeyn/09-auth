6ac978c7519835f50a140d7acbaea377eed2bfae
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-03-09 22:49:50 +0200

    serverApi fix

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-
 const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
 
 export const nextServer = axios.create({
--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,19 +1,22 @@
-import { api } from "@/app/api/api";
+// import axios from 'axios';
+import { nextServer } from "@/lib/api/api";
 import { User } from "@/types/user";
 import type { Note } from '@/types/note'
 import type { PostNote } from '@/types/note'
+
+const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 
 export type CheckSessionRequest = {
   success: boolean;
 };
 
 export const checkSession = async () => {
-  const res = await api.get('/auth/session');
+  const res = await nextServer.get<CheckSessionRequest>('/auth/session');
   return res.data.success;
 };
 
 export const getMe = async ( ): Promise<User> => {
-  const { data } = await api.get<User>('/users/me');
+  const { data } = await nextServer.get<User>('/users/me');
   return data;
 }
 
@@ -23,9 +26,12 @@ interface HTTPResponse {
 }
 
 export async function fetchNotes(query: string, page: number, tag?: string) {
-  const response = await api.get<HTTPResponse>(
+  const response = await nextServer.get<HTTPResponse>(
     '/notes',
     {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
       params: {
         search: query,
         page,
@@ -38,22 +44,37 @@ export async function fetchNotes(query: string, page: number, tag?: string) {
 }
 
 export async function createNote(note: PostNote): Promise<Note> {
-  const response = await api.post<Note>(
-    '/notes', note,
+  const response = await nextServer.post<Note>(
+    '/notes', note, 
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    }
   );
   return response.data;
 }
 
 export async function deleteNote(id: string): Promise<Note> {
-  const response = await api.delete<Note>(
+  const response = await nextServer.delete<Note>(
     `/notes/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    }
   );
   return response.data;
 }
 
 export async function fetchNoteById(id: string): Promise<Note> {
-  const response = await api.get<Note>(
-    `/notes/${id}`
+  const response = await nextServer.get<Note>(
+    `https://notehub-public.goit.study/api/notes/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    }
   );
   return response.data;
 }
