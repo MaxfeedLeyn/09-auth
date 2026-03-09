47070e4d2ad89debc9812ec67a43c61c9587c520
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-03-09 18:13:38 +0200

    Add zustand store for user

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -4,12 +4,20 @@ import { create } from 'zustand';
 // 1. Імпортуємо функцію
 import { persist } from 'zustand/middleware';
 import { PostNote } from '@/types/note';
+import { User } from '@/types/user';
 
 type PostNoteDraftStore = {
   draft: PostNote;
   setDraft: (note: PostNote) => void;
   clearDraft: () => void;
 };
+
+type AuthStore = {
+  user: User ;
+  isAuthenticated: boolean;
+  setUser: (value: User) => void;
+  clearIsAuthenticated: () => void;
+}
 
 const initialDraft: PostNote = {
   title: '',
@@ -27,6 +35,27 @@ export const useNoteDraftStore = create<PostNoteDraftStore>()(
     {
       name: 'note-draft',
       partialize: (state) => ({ draft: state.draft }),
+    },
+  ),
+);
+
+const initialUser: User = {
+  username: '',
+  email: '',
+  avatar: ''
+};
+
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      user: initialUser,
+      isAuthenticated: false,
+      setUser: (user) => set(() => ({ user, isAuthenticated: true })),
+      clearIsAuthenticated: () => set(() => ({ user: initialUser, isAuthenticated: false })),
+    }),
+    {
+      name: 'auth',
+      partialize: (state) => ({ user: state.user, isAuthenticated: state.isAuthenticated }),
     },
   ),
 );
